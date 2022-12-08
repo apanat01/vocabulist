20164932537c0702209b63030babcf72e69efff1
Jennifer Weng <jenniferweng@jennifers-mbp.lan>
2022-12-08 11:26:31 -0500

    translate test

--- routes/listRoutes.cjs
+++ routes/listRoutes.cjs
@@ -33,4 +33,42 @@ router.post("/list/addWord", async (req, res) => {
     
 })
 
+const baseURL = "https://od-api.oxforddictionaries.com/api/v2"
+const appId = '6428d6a9';
+const appKey = 'e76667ef9a16a48f2021a29896009dfa';
+const headers = {app_id: appId, app_key: appKey};
+const source_lang = "en";
+const target_lang = "es";
+const word = "hello"
+
+// test 
+router.get("/translate/word", async (req, res) => {
+
+    const word = req.query.word;
+    const url = `${baseURL}/translations/${source_lang}/${target_lang}/${word}`;
+    console.log(req);
+
+    console.log(word);
+    // const url = `https://od-api.oxforddictionaries.com/api/v2/translations/en/es/${word}`;
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "app_id": appId,
+            "app_key": appKey
+        },
+        redirect: "follow"
+    })
+    .then(res => res.text())
+    .then(data => { 
+        // console.log(data);
+        // console.log(data["results"])
+        // const lexicalEntries = data.results.lexicalEntries;
+        // console.log(lexicalEntries);
+
+        console.log(JSON.parse(data))
+    })
+    .catch(err => console.log(err));
+})
+
 module.exports = router;
